26462b006008504ab4ffe2202f8538d25b666412
mikhita <mikheilispiriani@yahoo.com>
2022-11-12 22:11:06 +0400

    I add styles and two sections of todo

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,77 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(()=> todos.value.sort((a,b)=>{
+  return b.createdAt - a.createdAt
+}))
+
+const addTodo = ()=>{}
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(()=>{
+  name.value = localStorage.getItem('name') || ''
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="app">
+
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name">
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input type="text" 
+              placeholder="Create ToDo" 
+              v-model="input_content"/>
+        <h4>Pick a category</h4>
+        <div class="options">
+
+            <label>
+              <input 
+                    type="radio" 
+                    name="category"
+                    id="category"
+                    value="business"
+                    v-model="input_category"/>
+              <span class="bubble business "></span>
+              <div>Business</div>
+            </label>
+
+            <label>
+              <input 
+                    type="radio" 
+                    name="category"
+                    id="category"
+                    value="personal"
+                    v-model="input_category"/>
+              <span class="bubble personal "></span>
+              <div>Personal</div>
+            </label>
+
+        </div>
+
+        <input type="submit" value="Add todo"/>
+      </form>
+    </section>
+
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
